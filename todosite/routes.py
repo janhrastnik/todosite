aa64a0f5e7d8da1c0a984c335ae23b846840f6cb
Jan <jan.hrastnik2@gmail.com>
2018-01-01 17:10:45 +0100

    Added the whole package

---
+++ todosite/routes.py
@@ -0,0 +1,48 @@
+import sqlite3
+from flask import render_template, request, redirect, url_for
+from todosite import app
+
+conn = sqlite3.connect('todolist.db')
+conn.text_factory = str
+c = conn.cursor()
+
+c.execute(""" CREATE TABLE IF NOT EXISTS todolist (
+				entry text
+	) """)
+conn.commit()
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+	idea = request.form['ideaInput']
+	c.execute("INSERT INTO todolist VALUES (:entry)", {'entry': idea})
+	conn.commit()
+	return redirect(url_for('table'))
+
+
+@app.route('/')
+@app.route('/index')
+def table():
+	c.execute("SELECT * FROM todolist")
+	todolist = [i[0] for i in c.fetchall()]
+	todoliststr = "".join(todolist)
+	conn.commit()
+	return render_template('todolist.html', todolist=todolist),  # todoliststr
+
+
+@app.route('/delete_entry', methods=['POST'])
+def delete_entry():
+	entryid = request.form['deleteEntry']
+	c.execute("DELETE FROM todolist WHERE entry = :entryid", {"entryid": entryid})
+	return redirect(url_for('table'))
+"""
+
+@app.route('/test', methods=['POST'])
+def test():
+	testentry = request.form['test']
+	return testentry
+
+
+@app.teardown_appcontext
+def close_db(error):
+	conn.close()
+"""
