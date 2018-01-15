efcac282fab10813723d3f0dd5980418c0b06c0d
JakobSefovSin <edward.alderson.mr.robot@gmail.com>
2018-01-15 16:09:01 +0100

    Manual merge

--- todosite/routes.py
+++ todosite/routes.py
@@ -1,56 +1,62 @@
 import sqlite3
 from flask import render_template, request, redirect, url_for
 from todosite import app
-
+from .forms import LoginForm
 
 conn = sqlite3.connect('todoList.db')
 conn.text_factory = str
 c = conn.cursor()
 
 c.execute(""" CREATE TABLE IF NOT EXISTS todoList (
-				entry text NOT NULL,
-				done integer NOT NULL
-	) """)
+                entry text NOT NULL,
+                done integer NOT NULL
+    ) """)
 conn.commit()
 
 @app.route('/')
 def index():
-	c.execute("SELECT rowid,* FROM todoList WHERE done = 0")
-	todoList = c.fetchall()
-	conn.commit()
+    c.execute("SELECT rowid,* FROM todoList WHERE done = 0")
+    todoList = c.fetchall()
+    conn.commit()
 
-	c.execute("SELECT rowid,* FROM todoList WHERE done = 1")
-	doneList = c.fetchall()
-	conn.commit()
+    c.execute("SELECT rowid,* FROM todoList WHERE done = 1")
+    doneList = c.fetchall()
+    conn.commit()
 
-	return render_template('index.html', todoList=todoList, doneList=doneList)
+    return render_template('index.html', todoList=todoList, doneList=doneList)
 
 @app.route('/handleData', methods=['POST'])
 def handleData():
-	idea = request.form['ideaInput']
-	if idea:
-		c.execute("INSERT INTO todoList VALUES (:entry, :done)", {'entry': idea, 'done': 0})
-		conn.commit()
-	return redirect(url_for('index'))
+    idea = request.form['ideaInput']
+    if idea:
+        c.execute("INSERT INTO todoList VALUES (:entry, :done)", {'entry': idea, 'done': 0})
+        conn.commit()
+    return redirect(url_for('index'))
 
 @app.route('/deleteEntry', methods=['POST'])
 def deleteEntry():
-	entryId = request.form['deleteId']
-	c.execute("DELETE FROM todoList WHERE rowid = :Id", {"Id": entryId})
-	conn.commit()
-	return redirect(url_for('index'))
+    entryId = request.form['deleteId']
+    c.execute("DELETE FROM todoList WHERE rowid = :Id", {"Id": entryId})
+    conn.commit()
+    return redirect(url_for('index'))
 
 @app.route('/doneEntry', methods=['POST'])
 def doneEntry():
-	entryId = request.form['doneId']
+    entryId = request.form['doneId']
 
-	c.execute("UPDATE todoList SET done = :done WHERE rowid = :Id", {"done": 1, "Id": entryId})
-	conn.commit()
+    c.execute("UPDATE todoList SET done = :done WHERE rowid = :Id", {"done": 1, "Id": entryId})
+    conn.commit()
 
-	return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/test')
 def test():
-	c.execute("SELECT * FROM todoList")
-	return str(c.fetchall())
+    c.execute("SELECT * FROM todoList")
+    return str(c.fetchall())
