21b4b24634d5258035ce38ce19989bd615935698
JakobSefovSin <edward.alderson.mr.robot@gmail.com>
2018-01-16 14:34:52 +0100

    Deletes unused js, users have personal todo lists.

--- todosite/routes.py
+++ todosite/routes.py
@@ -5,12 +5,15 @@ from .forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from .models import User
 from todosite import db
+from werkzeug.urls import url_parse
+
 conn = sqlite3.connect('todoList.db')
 conn.text_factory = str
 c = conn.cursor()
 
 c.execute(""" CREATE TABLE IF NOT EXISTS todoList (
                 entry text NOT NULL,
+                username text NOT NULL,
                 done integer NOT NULL
     ) """)
 conn.commit()
@@ -18,11 +21,11 @@ conn.commit()
 @app.route('/')
 @login_required
 def index():
-    c.execute("SELECT rowid,* FROM todoList WHERE done = 0")
+    c.execute("SELECT rowid, entry FROM todoList WHERE done = 0 AND username = :username", {'username':current_user.username})
     todoList = c.fetchall()
     conn.commit()
 
-    c.execute("SELECT rowid,* FROM todoList WHERE done = 1")
+    c.execute("SELECT rowid, entry FROM todoList WHERE done = 1 AND username = :username", {'username':current_user.username})
     doneList = c.fetchall()
     conn.commit()
 
@@ -32,7 +35,7 @@ def index():
 def handleData():
     idea = request.form['ideaInput']
     if idea:
-        c.execute("INSERT INTO todoList VALUES (:entry, :done)", {'entry': idea, 'done': 0})
+        c.execute("INSERT INTO todoList VALUES (:entry, :done, :username)", {'entry': idea, 'done': 0, 'username':current_user.username})
         conn.commit()
     return redirect(url_for('index'))
 
@@ -60,19 +63,19 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
+
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/test')
 def test():
-    c.execute("SELECT * FROM todoList")
-    return str(c.fetchall())
+    print(current_user.username)
+    return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
@@ -90,6 +93,5 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
